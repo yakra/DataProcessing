7ee2ce867bfcd7d78af4569c26865e4b83543cda
eric bryant <ericdbryant@yahoo.com>
2021-01-06 22:46:02 -0500

    _lctT4B
    https://github.com/TravelMapping/DataProcessing/issues/397#issuecomment-753732449
    restore 2nd hunk from _lct2
    
    Old "0.1x10" tests show this to be slightly slower on lab3; no discernible differences on other machines.
    
    Makes sense in that the old loop avoided assigning & incrementing lbegin.
    
    New "0.01x50" tests are expected to look similar. (MAYBE start to show diffs on other machines?)

--- siteupdate/cplusplus/classes/Route/label_and_connect.cpp
+++ siteupdate/cplusplus/classes/Route/label_and_connect.cpp
@@ -52,8 +52,8 @@ void Route::label_and_connect(ErrorList& el)
 		}
 		for (std::string& a : point_list[index]->alt_labels)
 		{	// create canonical AltLabels
-			while (a[0] == '+' || a[0] == '*') a = a.substr(1);
-			upper(a.data());
+			for (lbegin = a.data(); *lbegin == '+' || *lbegin == '*'; lbegin++);
+			a = upper(lbegin);
 			// populate unused set
 			unused_alt_labels.insert(a);
 			// create label->index hashes and check if AltLabels duplicated
